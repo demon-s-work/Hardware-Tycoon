7b6d835590190be63272cd913ba79c9e6786fc56
Envy <envy1396@gmail.com>
2023-12-03 02:16:41 +0300

    cpu creation added

---
+++ Scripts/Models/Architecture.cs
@@ -0,0 +1,21 @@
+﻿using Godot;
+
+namespace HardwareTycoon.Scripts.Models
+{
+	public partial class Architecture : GodotObject
+	{
+		public int Id { get; set; }
+		public string Name { get; set; }
+		public int InventionYear { get; set; }
+	}
+		/*
+		a9090,
+		x86,
+		ARM32,
+		ARM64,
+		AVR,
+		AVR32,
+		POWER,
+		EKARMA,
+*/
+}
---
+++ Scripts/Models/CpuModel.cs
@@ -0,0 +1,11 @@
+﻿using Godot;
+
+namespace HardwareTycoon.Scripts.Models
+{
+	public partial class CpuModel : GodotObject
+	{
+		public string Name { get; set; }
+		public int Cores { get; set; }
+		public Architecture Architecture { get; set; }
+	}
+}
